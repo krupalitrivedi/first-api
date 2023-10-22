03fe6050a93853faabbc3505c6a24abed42b5896
Krupali Trivedi <krupalitrivedi2002@gmail.com>
2023-10-23 02:12:15 +0530

    Changes and additional files

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,18 +1,28 @@
-const getUser = (req, res) => {
+const express = require('express');
+const UserToBeUsed = require('../models/userModel')
+
+const getUser = asyncHandler(async (req, res) => {
     res.status(200).json({message: "Get User"})
-}
+});
 
-const setUser = (req, res) => {
-    res.status(200).json({message: "Set User"})
-}
+const setUser = asyncHandler(async(req, res) => {
+    if(!req.body.id){
+        res.status(400);
+        throw new Error('Please add id');
+    }
+    const user = await UserToBeUsed.create({
+        id: req.body.id,
+    });
+    res.status(200).json({message: 'Set User'})
+});
 
-const updateUser = (req,res) => {
+const updateUser = asyncHandler(async(req,res) => {
     res.status(200).json({message: `Update user ${req.params.id}`})
-}
+});
 
-const deleteUser = (req, res) => {
+const deleteUser = asyncHandler(async(req, res) => {
     res.status(200).json({message: `Delete user ${req.params.id}`})
-}
+});
 
 module.exports = {
     getUser,
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const connectDB = require('../config/db')
 const user = require('./models/userModel')
+const {errorHandler} = require('./middleware/errorMiddleware');
 const port = 5000;
 connectDB();
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use('/api/user', require('./routes/userRoutes'))
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use('/api/user', require('./routes/userRoutes'));
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
